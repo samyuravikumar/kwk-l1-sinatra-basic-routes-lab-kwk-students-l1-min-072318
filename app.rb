237e3468485c2543bbda131273e70a31348935ae
samyuravikumar <samyuktha321@gmail.com>
2018-07-31 15:31:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Samyu"
+  end
+  
+  get '/hometown' do
+    "My hometown is Shakopee"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is All Too Well by Taylor Swift"
+  end
 end
